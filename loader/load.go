1121441f6c0912c23fda6217ab77c77d5eead9c4
chenhengqi <chenhengqi@outlook.com>
2021-03-13 10:30:56 +0800

    update go generate cmd

--- loader/load.go
+++ loader/load.go
@@ -1,3 +1,3 @@
 package loader
 
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc clang-11 network_drop ../network/drop.c
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc clang-11 network_drop ../network/drop.c -- -I..
